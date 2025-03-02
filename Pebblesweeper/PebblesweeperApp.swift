2cd6e3a1cab25c34e0449f94a76a8a93c02b8d21
Spenser <18bf4e7@proton.me>
2025-03-01 20:13:42 -0500

    Initial Commit

---
+++ Pebblesweeper/PebblesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PebblesweeperApp.swift
+//  Pebblesweeper
+//
+//  Created by Spenser Dubin on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct PebblesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PebblesweeperTests/PebblesweeperTests.swift
@@ -0,0 +1,17 @@
+//
+//  PebblesweeperTests.swift
+//  PebblesweeperTests
+//
+//  Created by Spenser Dubin on 3/1/25.
+//
+
+import Testing
+@testable import Pebblesweeper
+
+struct PebblesweeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
